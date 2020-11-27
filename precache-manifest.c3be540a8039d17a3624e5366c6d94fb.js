3fde4a0ac33f427240172ea98502d78c74775c4b
Cyril <cyril.cordier@epitech.eu>
2020-11-27 10:21:10 +0100

    Updates

---
+++ precache-manifest.c3be540a8039d17a3624e5366c6d94fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569dd0f9d9b707c90a18a0eed9f8d6ad",
+    "url": "/cyril-cordier/todolist-react/index.html"
+  },
+  {
+    "revision": "b6dd522b358473170250",
+    "url": "/cyril-cordier/todolist-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b6dd522b358473170250",
+    "url": "/cyril-cordier/todolist-react/static/js/2.b7fa8f2a.chunk.js"
+  },
+  {
+    "revision": "dfedcb64fd3ac4dd7930",
+    "url": "/cyril-cordier/todolist-react/static/js/main.4fec9bfc.chunk.js"
+  },
+  {
+    "revision": "ba6bb6d10d88b087a507",
+    "url": "/cyril-cordier/todolist-react/static/js/runtime~main.02c061ab.js"
+  }
+]);
