61aba9fbf47b95c79d57da0764cbee13df43c562
Cyril <cyril.cordier@epitech.eu>
2020-11-27 10:14:43 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Todo from "./Todo"
+
+function App() {
+  
+    return (
+      <div className="container">
+       <h1>Todo List</h1>
+       <Todo />
+      </div>
+    );
+  }
+
+export default App;
